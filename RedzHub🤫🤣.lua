9b4d767e7f18016f3264e2c7591d711ee7cd42db
Hieu594 <tranminhhieu2647@gmail.com>
2025-05-31 12:50:51 +0700

    Add files via upload

---
+++ RedzHub🤫🤣.lua
@@ -0,0 +1,20 @@
+shared.LoaderTitle = "Đăng Ký Kênh TBoy Roblox";
+shared.LoaderKeyFrames = {
+    [1] = {
+        1,
+        10
+    },
+    [2] = {
+        2,
+        30
+    },
+    [3] = {
+        3,
+        60
+    },
+    [4] = {
+        2,
+        100
+    }
+};
+local Settings = {  JoinTeam = "Pirates"; -- Pirates/Marines  Translator = true; -- true/false  }  loadstring(game:HttpGet("https://raw.githubusercontent.com/tlredz/Scripts/refs/heads/main/main.luau"))(Settings)  
